78ad7c82a1fa62b752378774e4d6e92b8d6d8f74
Wil Gerard <williamrgerard@gmail.com>
2021-12-01 16:56:15 -0600

    edit: bg color is now light blue | h1 color is now white

--- src/components/bg/index.js
+++ src/components/bg/index.js
@@ -4,15 +4,17 @@ import styles from './bg.module.scss';
 export default function Bg() {
 
     return (
-        <div className={styles.wrapper}>
-            <header className={styles.header}>
-                <h1>react runner</h1>
-            </header>
-            <a href="https://wil-gerard.github.io/react-runner/">
-                <footer className={styles.about}>
-                    <h2>about the game</h2>
-                </footer>
-            </a>
+        <div className={styles.background}>
+            <div className={styles.wrapper}>
+                <header className={styles.header}>
+                    <h1>react runner</h1>
+                </header>
+                <a href="https://wil-gerard.github.io/react-runner/">
+                    <footer className={styles.about}>
+                        <h2>about the game</h2>
+                    </footer>
+                </a>
+            </div>
         </div>
     );
 };
